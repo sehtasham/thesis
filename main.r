1f867ba698c47f0e49352ba1d596d81b13b21e87
sehtasham <s.ehtasham@yahoo.com>
2022-05-20 14:24:06 -0400

    Custom Updates

--- main.r
+++ main.r
@@ -14,9 +14,9 @@ time_to_offload_task <- function(D, R) {
 
 # For Communication Model TDMA
 P_t <- p_i
-G <- c(0.5, 1, 1.5) * (10 ^ -5) # Channel Gain different states
-h <- G[1]
-R_tdma <- upload_data_rate_device_tdma(W, sigma_2, h, P_t)
+G <- c(0.5, 1, 1.5) * (-100) # Channel Gain different states
+h <- G[2]
+R_tdma <- upload_data_rate_device_tdma(W, noise_power, h, P_t)
 to_tdma <- time_to_offload_task(D, R_tdma)
 
 # For Communication Model with AP
@@ -24,32 +24,32 @@ sigma <- 8 # Standard Deviation
 mean <- 0 # Mean
 d_km <- seq(0.1, 10, by = 0.1) # Sequence of distance in Km
 dev_power <- p_i
-R_ki <- upload_data_rate_device_ap(W, dev_power,
+R_ap <- upload_data_rate_device_ap(W, dev_power,
                                 noise_power, d_km[10], mean, sigma)
-to_ap <- time_to_offload_task(D, R_ki)
+to_ap <- time_to_offload_task(D, R_ap)
 
 # For communication model with UAV
 W_i <- W
 alpha_0 <- -50 #dBm
 L_i <- c(0, 0, 0)
-L_UAV <- c(1000, 0, 0)
+L_uav <- c(1, 0, 0)
 
-Ri_UAV <- upload_data_rate_device_uav(W_i, p_i, noise_power,
-                                alpha_0, L_UAV, L_i)
-to_uav <- time_to_offload_task(D, Ri_UAV)
+R_uav <- upload_data_rate_device_uav(W_i, p_i, noise_power,
+                                alpha_0, L_uav, L_i)
+to_uav <- time_to_offload_task(D, R_uav)
 
 
 # For Communication Model eNodeB
 P_i <- p_i
-L <- 100
+L <- 1
 h_i <- 127 + (30 * log10(L))
-theta_i <- 0.1
-R_node <- upload_data_rate_device_node(W, noise_power, h_i, P_i, theta_i)
+theta_i <- 0.5
+R_node <- upload_data_rate_device_node(W, sigma_2, h_i, P_i, theta_i)
 to_node <- time_to_offload_task(D, R_node)
 
 
 t_o <- to_tdma
-print(t_o)
+print(R_tdma)
 
 outer_expected_delay <- function(lambda, p_o) {
 expected_delay(D, v, p_o, f_l, f_e, t_o, lambda)
